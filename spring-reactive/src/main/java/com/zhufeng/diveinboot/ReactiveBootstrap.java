c0299fc65003345846655a143406d4429c0adf35
zhufeng8407 <zhufeng.zhu@163.com>
2019-07-20 00:19:11 +0800

    boot test

---
+++ spring-reactive/src/main/java/com/zhufeng/diveinboot/ReactiveBootstrap.java
@@ -0,0 +1,8 @@
+package com.zhufeng.diveinboot;
+
+public class ReactiveBootstrap {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ spring-reactive/src/main/java/com/zhufeng/diveinboot/reactive/FluxDemo.java
@@ -0,0 +1,8 @@
+package com.zhufeng.diveinboot.reactive;
+
+public class FluxDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
